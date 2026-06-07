6a454a097f95f29c994fafe5c9f28c01554c5cd6
DanishHaji <danish.haji111@gmail.com>
2026-06-07 14:26:09 +0500

    updated pdf

--- app/lib/pdf.ts
+++ app/lib/pdf.ts
@@ -1,7 +1,25 @@
-import pdfParse from "pdf-parse";
+import * as pdfjsLib from "pdfjs-dist";
 
-export async function extractTextFromPDF(file: Buffer) {
-  const data = await pdfParse(file);
+export async function extractTextFromPDF(buffer: Buffer) {
+  const uint8Array = new Uint8Array(buffer);
 
-  return data.text;
+  const pdf = await pdfjsLib.getDocument({
+    data: uint8Array,
+  }).promise;
+
+  let fullText = "";
+
+  for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+    const page = await pdf.getPage(pageNum);
+
+    const textContent = await page.getTextContent();
+
+    const pageText = textContent.items
+      .map((item: any) => ("str" in item ? item.str : ""))
+      .join(" ");
+
+    fullText += pageText + "\n";
+  }
+
+  return fullText;
 }
